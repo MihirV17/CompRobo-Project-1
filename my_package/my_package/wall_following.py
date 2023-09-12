30ea4ef7b018a19173e1589ce0938eae817523c0
kenta Terasaki <kterasaki@olin.edu>
2023-09-12 02:17:24 -0400

    Wall tracking code

--- my_package/my_package/wall_following.py
+++ my_package/my_package/wall_following.py
@@ -1,43 +1,38 @@
 import rclpy
-from rclpy.node import Node
+from rclpy.node import Node, Publisher
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Vector3
+
 
 class WallFollow(Node):
+    forward_distance = 0
+    backward_distane = 0
     def __init__(self):
         super().__init__('wall_follow')
-        self.sub = self.create_subscription(LaserScan, '/scan', self.laser_callback, 10)
-        self.pub = self.create_publisher(Twist, '/cmd_vel', 10)
-        self.target_distance = 0.5  # Desired distance from the wall
+        # Create a timer that fires ten times per second
+        timer_period = 0.1
+        self.timer = self.create_timer(timer_period, self.run_loop)
+        self.neato_pub: Publisher = self.create_publisher(Twist, "cmd_vel", 10)
+        self.scan_sub = self.create_subscription(LaserScan, "stable_scan", self.scan_callback, 10)
 
-    def laser_callback(self, msg):
-        # Define the range index for the direction you want to follow the wall (e.g., 0 degrees)
-        target_index = 0
-        desired_distance = self.target_distance
+    def scan_callback(self, scan: LaserScan):
+        self.forward_distance = scan.ranges[45]
+        self.backward_distane = scan.ranges[135]
 
-        if not msg.ranges:
-            return
-
-        if msg.ranges[target_index] > desired_distance:
-            # If the distance to the wall is greater than the desired distance, turn towards it
-            angular_z = -0.2  # Adjust the angular velocity as needed
-            linear_x = 0.0
-        else:
-            # If the distance to the wall is less than the desired distance, turn away from it
-            angular_z = 0.2  # Adjust the angular velocity as needed
-            linear_x = 0.0
-
-        # Create and publish a Twist message to control the robot's motion
-        twist_msg = Twist()
-        twist_msg.linear.x = linear_x
-        twist_msg.angular.z = angular_z
-        self.pub.publish(twist_msg)
-
+    def run_loop(self):
+        angular_speed = (self.forward_distance - self.backward_distane) / 2
+        if angular_speed > 1:
+            angular_speed = 1
+        elif angular_speed < -1:
+            angular_speed = -1
+        print(angular_speed)     
+        msg: Twist = Twist(angular=Vector3(x=0,y=0,z=angular_speed),linear=Vector3(x=.5,y=0,z=0))
+        self.neato_pub.publish(msg)
 def main(args=None):
     rclpy.init(args=args)
     node = WallFollow()
     rclpy.spin(node)
-    node.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
